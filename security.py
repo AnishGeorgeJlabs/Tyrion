7c7703d57887b89d3d0633dc31466faf26fea9ff
AnishGeorgeJlabs <anish.george@jlabs.co>
2015-10-20 17:04:40 +0530

    Added credentials

--- security.py
+++ security.py
@@ -1,5 +1,6 @@
 from django.views.decorators.csrf import csrf_exempt
 
+from bson.json_util import ObjectId
 from . import db, get_json, basic_failure, basic_error, basic_success
 
 @csrf_exempt
@@ -11,9 +12,41 @@ def login(request):
 
     res = db.credentials.find_one({"username": data['username'], "password": data['password']})
     if res:
+        vendor_id = int(res['vendor_id'])
+        mer = db.merchants.find_one({"vendors.vendor_id": vendor_id}, {"vendors.$": True, "name": True})
         return basic_success({
-            "vendor_id": int(res['vendor_id']),
-            "api_key": str(res['_id'])
+            "vendor_id": vendor_id,
+            "api_key": str(res['_id']),
+            "name": mer['name'],
+            "address": mer['vendors'][0]['address']
         })
     else:
         return basic_failure("Unauthorized access")
+
+@csrf_exempt
+def auth(handler):
+    """ Authorization layer for merchant application
+    :param handler: A function which will take 2 parameters (options, vendor_id) and return JSON response
+    :return: the handler function wrapped with the authorization middleware
+    """
+    def authorized_access(request):
+        if request.method == "GET":
+            opts = request.GET
+        else:
+            opts = get_json(request)
+
+        for key in ['api_key', 'vendor_id']:
+            if key not in opts:
+                return basic_error(key+" missing, unauthorized access")
+
+        api_key = opts.pop('api_key')
+        vendor_id = int(opts.pop('vendor_id'))
+        try:
+            if db.credentials.count({"_id": ObjectId(api_key), "vendor_id": vendor_id}) > 0:
+                return handler(opts, vendor_id)
+            else:
+                return basic_failure("Unauthorized access")
+        except Exception as e:
+            return basic_error("Authorization error: "+str(e))
+
+    return authorized_access
